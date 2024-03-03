ad5497c34f7286523116b4c4eb1111235f18004e
ingointo <vishnuvishnumohanms@gmail.com>
2024-03-03 15:41:18 +0530

    added bottom sheet

--- App.js
+++ App.js
@@ -1,17 +1,24 @@
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Toast from 'react-native-toast-message';
 import CustomToast from '@components/Toast/CustomToast';
+import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
+import { GestureHandlerRootView } from 'react-native-gesture-handler'; // Import GestureHandlerRootView
 import StackNavigator from '@navigation/StackNavigator';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <SafeAreaProvider>
-       <StackNavigator/>
-      </SafeAreaProvider>
-      <Toast config={CustomToast} />
-    </NavigationContainer>
+    
+    <GestureHandlerRootView style={{flex:1}}>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <BottomSheetModalProvider>
+            <StackNavigator />
+          </BottomSheetModalProvider>
+          <Toast config={CustomToast} />
+        </SafeAreaProvider>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 }
-
--- src/screens/Home/HomeScreen.js
+++ src/screens/Home/HomeScreen.js
@@ -1,23 +1,31 @@
-import React from 'react';
-import { View, Image, Dimensions, TouchableOpacity} from 'react-native';
+import React, { useMemo } from 'react';
+import { View, Image, Dimensions, TouchableOpacity, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import THEME from '@constants/theme';
 import Text from '@components/Text';
 import CarouselPagination from '@components/Home/CarouselPagination';
-
+import BottomSheet from "@gorhom/bottom-sheet";
 
 const { width } = Dimensions.get('window');
 
 const HomeScreen = ({ navigation }) => {
 
+  const navigateToScreen = (screenName) => {
+    navigation.navigate(screenName);
+  };
+
+  const snapPoints = useMemo(() => ["50%", "85%"], []);
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: THEME.COLORS.appTheme }}>
       <View style={{ flex: 1, backgroundColor: 'white', borderTopLeftRadius: 15, borderTopRightRadius: 15 }}>
+
         {/* Header */}
         <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingVertical: 10 }}>
           <Image source={require('@assets/images/Home/Header/header_transparent_bg.png')} style={{ width: width * 0.5, aspectRatio: 3 }} />
           <Image source={require('@assets/images/Home/Header/notification.gif')} style={{ width: width * 0.25, aspectRatio: 2 / 1, resizeMode: 'contain' }} />
         </View>
+
         {/* Navigation Header */}
         <View style={{ backgroundColor: '#2e294e', padding: 10, marginHorizontal: 20, borderRadius: 10, justifyContent: 'space-between', flexDirection: 'row', alignItems: 'center', paddingHorizontal: 18 }}>
           <TouchableOpacity onPress={() => navigation.navigate('SearchProducts')}>
@@ -30,11 +38,65 @@ const HomeScreen = ({ navigation }) => {
             <Image source={require('@assets/images/Home/Header/barcode_scanner.png')} style={{ width: 20, height: 20 }} tintColor="white" />
           </TouchableOpacity>
         </View>
+
         {/* Carousel */}
-        <CarouselPagination/>
-      </View>
+        <CarouselPagination />
+
+        {/* Section */}
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginHorizontal: 8 }}>
+          <ImageContainer source={require('@assets/images/Home/section/pickup.png')} onPress={() => navigateToScreen('Pickup')} backgroundColor="#f37021" title="Pickup" />
+          <ImageContainer source={require('@assets/images/Home/section/services.png')} onPress={() => navigateToScreen('Services')} backgroundColor="#f37021" title="Services" />
+          <ImageContainer source={require('@assets/images/Home/section/customer.png')} onPress={() => navigateToScreen('Customer')} backgroundColor="#f37021" title="Customer" />
+        </View>
+        {/* Bottom sheet */}
+          <BottomSheet snapPoints={snapPoints}>
+            <View style={{
+              flex: 1,
+              alignItems: "center",
+            }}>
+            </View>
+          </BottomSheet>
+        </View>
     </SafeAreaView>
   );
 }
+
+const ImageContainer = ({ source, onPress, backgroundColor, title }) => (
+  <View style={styles.imageContainer}>
+    <Image source={source} style={styles.image} />
+    <TouchableOpacity style={[styles.buttonContainer, { backgroundColor }]} onPress={onPress}>
+      <Text style={styles.buttonText}>{title}</Text>
+    </TouchableOpacity>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  imageContainer: {
+    height: 100,
+    width: width * 0.3,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: 'black',
+    borderWidth: 0.5,
+    borderRadius: 10
+  },
+  image: {
+    width: width * 0.25,
+    height: width * 0.11,
+    resizeMode: 'contain',
+  },
+  buttonContainer: {
+    width: '85%',
+    paddingVertical: 5,
+    borderRadius: 5,
+    alignItems: 'center',
+    // marginVertical:5
+    marginTop: 5,
+  },
+  buttonText: {
+    color: THEME.COLORS.white,
+    fontFamily: THEME.FONT_FAMILY.urbanistBold
+  },
+});
 
 export default HomeScreen;
