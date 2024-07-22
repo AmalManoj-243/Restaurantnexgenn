965cf05e5774de69ae08123171f3a597c4c9df39
ingointo <vishnuvishnumohanms@gmail.com>
2024-07-22 13:02:39 +0530

    updated uae build

--- src/api/config/apiConfig.js
+++ src/api/config/apiConfig.js
@@ -1,7 +1,7 @@
 // src/api/config.js
 
 {/* ----Shansapp live(UAE) ----- */ }
-// const API_BASE_URL = 'https://shansapp.369ai.biz:3004';
+const API_BASE_URL = 'https://shansapp.369ai.biz:3004';
 
 {/* ----UAE Test ----- */ }
 // const API_BASE_URL = 'https://uaetest.369ai.biz:3027'
@@ -10,7 +10,7 @@
 // const API_BASE_URL = 'https://test.369ai.biz:3016' //backend test
 
 {/* ----Oman live ----- */ }
-const API_BASE_URL = 'https://danatoman.369ai.biz:3029' //oman live
+// const API_BASE_URL = 'https://danatoman.369ai.biz:3029' //oman live
 
 
 export default API_BASE_URL;
